46472f6e682a436da39f653be9248e36001a7b67
“Moises” <moises.soares@gmail.com>
2024-05-17 22:45:52 +0100

    sitemapscrawler done

--- cettire-sitemaps-scraper/app.js
+++ cettire-sitemaps-scraper/app.js
@@ -2,18 +2,19 @@ const axios = require('axios');
 const xml2js = require('xml2js');
 const { Kafka } = require('kafkajs');
 
-// Kafka configuration using environment variable
-const kafkaBroker = process.env.KAFKA_BROKER || 'kafka:9092';
+
+const kafkaBroker = process.env.KAFKA_BROKER || 'localhost:29092';
 const kafka = new Kafka({
     clientId: 'sitemap-crawler',
     brokers: [kafkaBroker]
 });
-const producer = kafka.producer();
-const topic = 'product-urls';
 
-async function downloadFile(url) {
+const producer = kafka.producer();
+const target_topic = 'product-urls';
+
+async function downloadSitemap(url) {
     try {
-        console.log(`Downloading ${url}...`);
+        console.log(`Downloading sitemap  ${url} ...`);
         const response = await axios.get(url);
         return response.data;
     } catch (error) {
@@ -22,11 +23,11 @@ async function downloadFile(url) {
     }
 }
 
-async function parseSitemaps(xmlData) {
+async function getUrlsFromMainSitemap(xmlData) {
     try {
+        console.log('Parsing XML data  from main sitemap ...');
         const parser = new xml2js.Parser({ explicitArray: false });
         const result = await parser.parseStringPromise(xmlData);
-
         const sitemaps = result.sitemapindex.sitemap;
         return sitemaps.map(sitemap => sitemap.loc);
     } catch (error) {
@@ -35,21 +36,19 @@ async function parseSitemaps(xmlData) {
     }
 }
 
-async function downloadCettireSitemaps() {
+async function getCettireSitemaps() {
     const mainSitemapUrl = 'https://www.cettire.com/sitemap.xml';
     try {
-        // Download the main sitemap
-        const mainSitemapData = await downloadFile(mainSitemapUrl);
-        const sitemapUrls = await parseSitemaps(mainSitemapData);
-
-        // Process each sitemap URL found
+        console.log(`Processing main sitemap: ${mainSitemapUrl}`);
+        const mainSitemapData = await downloadSitemap(mainSitemapUrl);
+        const sitemapUrls = await getUrlsFromMainSitemap(mainSitemapData);
+        console.log(`Found ${sitemapUrls.length} sitemaps. Start processing ...`);
         const sitemapDataArray = await Promise.all(
             sitemapUrls.map(async (url) => {
-                const data = await downloadFile(url);
+                const data = await downloadSitemap(url);
                 return { url, data };
             })
         );
-
         return sitemapDataArray.filter(item => item.data);
     } catch (error) {
         console.error(`Failed to process main sitemap: ${error.message}`);
@@ -58,32 +57,29 @@ async function downloadCettireSitemaps() {
 }
 
 const publishProductUrlFromSitemaps = async (sitemapDataArray) => {
+    console.log('Publishing product URLs to Kafka ...');
     await producer.connect();
-
     for (const { url, data } of sitemapDataArray) {
         const parser = new xml2js.Parser();
         const result = await parser.parseStringPromise(data);
-
-        // Extract URLs and publish them to Kafka
         const urlset = result.urlset.url;
         for (const url of urlset) {
             const message = {
-                value: url.loc.toString()  // Ensure the value is a string
+                value: url.loc.toString() 
             };
-            
-            console.log(`Publishing URL: ${message.value}`);
-
-     
-
+             producer.send({
+                 topic: target_topic,
+                 messages: [message]
+             }).then(r => console.log(`Published: ${message.value}`));
         }
     }
-
     await producer.disconnect();
 };
 
 (async () => {
     try {
-        const sitemapDataArray = await downloadCettireSitemaps();
+        console.log('Starting sitemap processing ...');
+        const sitemapDataArray = await getCettireSitemaps();
         await publishProductUrlFromSitemaps(sitemapDataArray);
     } catch (error) {
         console.error(`An error occurred: ${error.message}`);
